edf8842fc0337e4c3c82d73ce1d805e8fa00d9eb
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-10-18 09:21:28 +0100

    Latest

---
+++ lab1/Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle {
+    public static void main(String[] args){
+        for (int i = 0; i < 5; i++){
+            int j = 0;
+            while (j <= i) {
+                System.out.print('*');
+                j += 1;
+            }
+            System.out.println();
+        }
+    }
+}
