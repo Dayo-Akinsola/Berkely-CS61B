ec4fd26fa1e4748ba7446fd10640eb5b8c69a497
Dayo Akinsola <dayoakinsola3@gmail.com>
2024-05-06 18:02:53 +0100

    Removing Exception throw

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -11,13 +11,13 @@ public class Main {
      * Usage: java gitlet.Main ARGS, where ARGS contains
      * <COMMAND> <OPERAND1> <OPERAND2> ...
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 //        if (args.length == 0) {
 //            System.out.println("Please enter a command.");
 //            System.exit(0);
 //        }
         String firstArg = args[0];
-        switch (firstArg) {
+        switch(firstArg) {
             case "init":
 //                if (args.length != 2) {
 //                    printMessageAndExit("Incorrect operands.");
