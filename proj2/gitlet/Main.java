c494504a16b28b56669ba6ac9ad1f478b54d97f6
Dayo Akinsola <dayoakinsola3@gmail.com>
2024-05-06 18:16:16 +0100

    Add more explicit exception handling in Main

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,8 +1,10 @@
 package gitlet;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.Objects;
 
-import static gitlet.Repository.init;
+import static gitlet.Repository.*;
 import static gitlet.Utils.printMessageAndExit;
 
 /**
@@ -34,89 +36,117 @@ public class Main {
                     System.exit(0);
                 }
                 break;
-//            case "add":
-//                isInGitletDirectory();
-//                if (args.length != 2) {
-//                    printMessageAndExit("Incorrect operands.");
-//                }
-//                add(args[1]);
-//                break;
-//            case "commit":
-//                isInGitletDirectory();
-//                if (args.length != 2) {
-//                    printMessageAndExit("Incorrect operands.");
-//                }
-//                final String commitMessage = args[1];
-//                commit(commitMessage);
-//                break;
-//            case "rm":
-//                isInGitletDirectory();
-//                if (args.length != 2) {
-//                    printMessageAndExit("Incorrect operands.");
-//                }
-//                remove(args[1]);
-//                break;
-//            case "log":
-//                isInGitletDirectory();
-//                if (args.length != 2) {
-//                    printMessageAndExit("Incorrect operands.");
-//                }
-//                log();
-//                break;
-//            case "global-log":
-//                isInGitletDirectory();
-//                if (args.length != 2) {
-//                    printMessageAndExit("Incorrect operands.");
-//                }
-//                globalLog();
-//                break;
-//            case "find":
-//                isInGitletDirectory();
-//                if (args.length != 2) {
-//                    printMessageAndExit("Incorrect operands.");
-//                }
-//                final String message = args[1];
-//                find(message);
-//                break;
-//            case "status":
-//                isInGitletDirectory();
-//                if (args.length != 2) {
-//                    printMessageAndExit("Incorrect operands.");
-//                }
-//                status();
-//                break;
-//            case "checkout":
-//                isInGitletDirectory();
-//                if (args.length == 3 && Objects.equals(args[1], "--")) {
-//                    final String fileName = args[2];
-//                    checkoutFile(fileName);
-//                    return;
-//                }
-//
-//                if (args.length == 4 && Objects.equals(args[2], "--")) {
-//                    final String commitHash = args[1];
-//                    final String fileName = args[3];
-//                    checkoutCommitFile(commitHash, fileName);
-//                    return;
-//                }
-//
-//                if (args.length == 2) {
-//                    final String branchName = args[1];
-//                    checkoutBranch(branchName);
-//                    return;
-//                }
-//
-//                printMessageAndExit("Incorrect operands.");
-//                break;
-//            case "branch":
-//                isInGitletDirectory();
-//                if (args.length == 2) {
-//                    final String branchName = args[1];
-//                    branch(branchName);
-//                }
-//                break;
+            case "add":
+                isInGitletDirectory();
+                if (args.length != 2) {
+                    printMessageAndExit("Incorrect operands.");
+                }
+                try {
+                    add(args[1]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "commit":
+                isInGitletDirectory();
+                if (args.length != 2) {
+                    printMessageAndExit("Incorrect operands.");
+                }
+                final String commitMessage = args[1];
+                try {
+                    commit(commitMessage);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "rm":
+                isInGitletDirectory();
+                if (args.length != 2) {
+                    printMessageAndExit("Incorrect operands.");
+                }
+                try {
+                    remove(args[1]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "log":
+                isInGitletDirectory();
+                if (args.length != 2) {
+                    printMessageAndExit("Incorrect operands.");
+                }
+                try {
+                    log();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "global-log":
+                isInGitletDirectory();
+                if (args.length != 2) {
+                    printMessageAndExit("Incorrect operands.");
+                }
+                try {
+                    globalLog();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "find":
+                isInGitletDirectory();
+                if (args.length != 2) {
+                    printMessageAndExit("Incorrect operands.");
+                }
+                final String message = args[1];
+                find(message);
+                break;
+            case "status":
+                isInGitletDirectory();
+                if (args.length != 2) {
+                    printMessageAndExit("Incorrect operands.");
+                }
+                status();
+                break;
+            case "checkout":
+                isInGitletDirectory();
+                if (args.length == 3 && Objects.equals(args[1], "--")) {
+                    final String fileName = args[2];
+                    try {
+                        checkoutFile(fileName);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return;
+                }
+
+                if (args.length == 4 && Objects.equals(args[2], "--")) {
+                    final String commitHash = args[1];
+                    final String fileName = args[3];
+                    try {
+                        checkoutCommitFile(commitHash, fileName);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return;
+                }
+
+                if (args.length == 2) {
+                    final String branchName = args[1];
+                    checkoutBranch(branchName);
+                    return;
+                }
+
+                printMessageAndExit("Incorrect operands.");
+                break;
+            case "branch":
+                isInGitletDirectory();
+                if (args.length == 2) {
+                    final String branchName = args[1];
+                    branch(branchName);
+                }
+                break;
             default:
-//                printMessageAndExit("No command with that name exists.");
+                printMessageAndExit("No command with that name exists.");
                 break;
         }
 
